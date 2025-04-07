e8022f7c2968a2dd9b5cfc19aaac6c55bcccfbf7
Axel Lindberg <axel6@kth.se>
2025-04-07 14:57:25 +0200

    Basic methods and structure

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,26 @@
 public class Task {
+    private String name;
+    private boolean isDone;
 
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        this.isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + (isDone ? "Done" : "Not Done");
+    }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,3 +1,26 @@
-public class TaskManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(String name) {
+        tasks.add(new Task(name, false));
+    }
+
+    public boolean removeTask(Task task) {
+        return tasks.remove(task);
+    }
+
+    public void markTaskAsDone(Task task) {
+        task.setDone(true);
+    }
+
+    public void clearTasks() {
+        tasks.clear();
+    }
 }
